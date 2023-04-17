1c634b8bf14bd59ca08543c49facd1b167e39d80
foresuke <foresukecom@gmail.com>
2023-04-17 23:36:40 +0000

    feat: グローバルIP取得機能追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
+import IpAddress from '../views/IpAddress.vue'; // ここに新しいコンポーネントをインポート
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomePage,
+  },
+  {
+    path: '/ip-address',
+    name: 'ip-address',
+    component: IpAddress, // ここで新しいコンポーネントを指定
   },
   // 他のルートをここに追加
 ];
---
+++ src/views/IpAddress.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>IP Address</h1>
+    <p>Your IP address is: {{ ipAddress }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ipAddress: '',
+    };
+  },
+  async mounted() {
+    try {
+      const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+      this.ipAddress = data.ip;
+    } catch (error) {
+      console.error('Error fetching IP address:', error);
+    }
+  },
+};
+</script>
