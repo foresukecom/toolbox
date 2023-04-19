b34f707c3e01b3c11d3b062dbd11d4cbd15c202d
foresuke <foresukecom@gmail.com>
2023-04-19 22:43:20 +0900

    feat: ハッシュアルゴリズムを追加

--- src/views/TextHash.vue
+++ src/views/TextHash.vue
@@ -1,59 +1,72 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-textarea
-          v-model="textToHash"
-          label="Enter your text"
-          auto-grow
-        ></v-textarea>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-textarea
-          v-model="hashedText"
-          label="Hashed text"
-          auto-grow
-          readonly
-        ></v-textarea>
-      </v-col>
-    </v-row>
-    <v-btn @click="copyToClipboard" color="primary">Copy to clipboard</v-btn>
-  </v-container>
+  <div>
+    <v-textarea label="Input Text" v-model="inputText"></v-textarea>
+    <div class="hash-container">
+      <v-textarea label="MD5" readonly v-model="md5Hash"></v-textarea>
+      <a href="#" @click.prevent="copyToClipboard(md5Hash)">Copy MD5</a>
+    </div>
+    <div class="hash-container">
+      <v-textarea label="SHA-1" readonly v-model="sha1Hash"></v-textarea>
+      <a href="#" @click.prevent="copyToClipboard(sha1Hash)">Copy SHA-1</a>
+    </div>
+    <div class="hash-container">
+      <v-textarea label="SHA-256" readonly v-model="sha256Hash"></v-textarea>
+      <a href="#" @click.prevent="copyToClipboard(sha256Hash)">Copy SHA-256</a>
+    </div>
+    <div class="hash-container">
+      <v-textarea label="SHA-512" readonly v-model="sha512Hash"></v-textarea>
+      <a href="#" @click.prevent="copyToClipboard(sha512Hash)">Copy SHA-512</a>
+    </div>
+    <div class="hash-container">
+      <v-textarea label="SHA-3" readonly v-model="sha3Hash"></v-textarea>
+      <a href="#" @click.prevent="copyToClipboard(sha3Hash)">Copy SHA-3</a>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
-import { SHA256 } from 'crypto-js';
+import { MD5, SHA1, SHA256, SHA512, SHA3 } from 'crypto-js';
 
 export default {
-  setup() {
-    const textToHash = ref('');
-    const hashedText = ref('');
-
-    // textToHash が変更されたときに実行される関数
-    const onTextToHashChange = () => {
-      hashedText.value = SHA256(textToHash.value).toString();
-    };
-
-    // textToHash の変更を監視
-    watch(textToHash, onTextToHashChange);
-
-    const copyToClipboard = () => {
-      const textarea = document.createElement('textarea');
-      textarea.textContent = hashedText.value;
-      textarea.style.position = 'absolute';
-      textarea.style.left = '-9999px';
-      document.body.appendChild(textarea);
-      textarea.select();
-      document.execCommand('copy');
-      document.body.removeChild(textarea);
-    };
-
+  name: 'TextHash',
+  data() {
     return {
-      textToHash,
-      hashedText,
-      copyToClipboard,
+      inputText: '',
+      md5Hash: '',
+      sha1Hash: '',
+      sha256Hash: '',
+      sha512Hash: '',
+      sha3Hash: '',
     };
+  },
+  watch: {
+    inputText() {
+      this.md5Hash = this.hashText(MD5);
+      this.sha1Hash = this.hashText(SHA1);
+      this.sha256Hash = this.hashText(SHA256);
+      this.sha512Hash = this.hashText(SHA512);
+      this.sha3Hash = this.hashText(SHA3);
+    },
+  },
+  methods: {
+    hashText(hashFunction) {
+      return hashFunction(this.inputText).toString();
+    },
+    async copyToClipboard(text) {
+      try {
+        await navigator.clipboard.writeText(text);
+        console.log('Copied to clipboard');
+      } catch (err) {
+        console.error('Failed to copy text: ', err);
+      }
+    },
   },
 };
 </script>
+
+<style scoped>
+.hash-container {
+  display: flex;
+  align-items: center;
+}
+</style>
