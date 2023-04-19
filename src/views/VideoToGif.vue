38c5873879aec1658ec3ec8ec2cc3bfa01312e9b
foresuke <foresukecom@gmail.com>
2023-04-20 00:40:50 +0900

    feat: gif変換時にサイズを指定可能にする

--- src/views/VideoToGif.vue
+++ src/views/VideoToGif.vue
@@ -1,47 +1,68 @@
 <template>
   <div>
-    <h2>Video to GIF Converter</h2>
-    <input type="file" @change="onFileSelected" accept="video/*" />
-    <br />
-    <button @click="convertVideoToGif" :disabled="!videoFile">Convert</button>
-    <br />
-    <img v-if="gifUrl" :src="gifUrl" alt="Generated GIF" />
-    <br />
-    <button v-if="gifUrl" @click="fetchFile">Download GIF</button>
+    <h1>動画からGIFへの変換</h1>
+    <input type="file" @change="loadVideo" accept="video/mp4,video/x-flv" />
+    <video v-if="videoSrc" controls :src="videoSrc"></video>
+    <div>
+      <label for="gifSize">GIFサイズ:</label>
+      <select id="gifSize" v-model="selectedGifSize">
+        <option v-for="size in gifSizes" :key="size" :value="size">{{ size }}</option>
+      </select>
+    </div>
+    <div>
+      <label for="frameRate">フレームレート:</label>
+      <select id="frameRate" v-model="selectedFrameRate">
+        <option v-for="rate in frameRates" :key="rate" :value="rate">{{ rate }}</option>
+      </select>
+    </div>
+    <button @click="convertVideoToGif">GIFに変換</button>
+    <img v-if="gifSrc" :src="gifSrc" />
+    <a v-if="gifSrc" :href="gifSrc" download="converted.gif">GIFをダウンロード</a>
   </div>
 </template>
 
 <script>
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-
-const ffmpeg = createFFmpeg({ log: true });
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 export default {
+  name: 'VideoToGif',
   data() {
     return {
-      videoFile: null,
-      gifUrl: "",
+      videoSrc: null,
+      gifSrc: null,
+      gifSizes: [320, 480, 640],
+      selectedGifSize: 320,
+      frameRates: [10, 15, 24],
+      selectedFrameRate: 10,
     };
   },
   methods: {
-    async onFileSelected(event) {
-      this.videoFile = event.target.files[0];
+    async loadVideo(event) {
+      const file = event.target.files[0];
+      if (!file) return;
+
+      const fileURL = URL.createObjectURL(file);
+      this.videoSrc = fileURL;
     },
     async convertVideoToGif() {
-      if (!this.videoFile) return;
-
+      const ffmpeg = createFFmpeg({ log: true });
       await ffmpeg.load();
-      ffmpeg.FS("writeFile", "input.mp4", await fetchFile(this.videoFile));
-      await ffmpeg.run("-i", "input.mp4", "-vf", "fps=10,scale=320:-1:flags=lanczos", "-c:v", "gif", "output.gif");
-      const data = ffmpeg.FS("readFile", "output.gif");
-      const url = URL.createObjectURL(new Blob([data.buffer], { type: "image/gif" }));
-      this.gifUrl = url;
-    },
-    fetchFile() {
-      const link = document.createElement("a");
-      link.href = this.gifUrl;
-      link.download = "output.gif";
-      link.click();
+      const videoFile = await fetchFile(this.videoSrc);
+      ffmpeg.FS('writeFile', 'input.mp4', videoFile);
+
+      await ffmpeg.run(
+        '-i',
+        'input.mp4',
+        '-vf',
+        `fps=${this.selectedFrameRate},scale=${this.selectedGifSize}:-1`,
+        '-f',
+        'gif',
+        'output.gif'
+      );
+
+      const data = ffmpeg.FS('readFile', 'output.gif');
+      const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+      this.gifSrc = url;
     },
   },
 };
