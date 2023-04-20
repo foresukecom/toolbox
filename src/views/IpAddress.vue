70fa0103bb8903d5f5b5ff8ce9aae6096dac1d7b
foresuke <foresukecom@gmail.com>
2023-04-21 08:47:17 +0900

    feat: アクセス元情報を追加

--- src/views/IpAddress.vue
+++ src/views/IpAddress.vue
@@ -2,6 +2,16 @@
   <div>
     <h1>IP Address</h1>
     <p>Your IP address is: {{ ipAddress }}</p>
+    <h2>User Agent Information</h2>
+    <p>User Agent: {{ userAgent }}</p>
+    <p>Language: {{ language }}</p>
+    <h2>Screen Information</h2>
+    <p>Screen Resolution: {{ screenWidth }} x {{ screenHeight }}</p>
+    <p>Viewport Size: {{ viewportWidth }} x {{ viewportHeight }}</p>
+
+    <p>Browser: {{ browser }}</p>
+    <p>Operating System: {{ os }}</p>
+
   </div>
 </template>
 
@@ -10,9 +20,60 @@ export default {
   data() {
     return {
       ipAddress: '',
+      userAgent: navigator.userAgent,
+      language: '',
+      screenWidth: 0,
+      screenHeight: 0,
+      viewportWidth: 0,
+      viewportHeight: 0
     };
   },
+  methods: {
+    parseUserAgent() {
+      const userAgent = navigator.userAgent;
+      let browser = 'Unknown Browser';
+      let os = 'Unknown OS';
+
+      // ブラウザの判定
+      if (userAgent.includes('Chrome')) {
+        browser = 'Google Chrome';
+      } else if (userAgent.includes('Firefox')) {
+        browser = 'Mozilla Firefox';
+      } else if (userAgent.includes('Safari')) {
+        browser = 'Apple Safari';
+      } else if (userAgent.includes('MSIE') || userAgent.includes('Trident')) {
+        browser = 'Internet Explorer';
+      } else if (userAgent.includes('Edge')) {
+        browser = 'Microsoft Edge';
+      } else if (userAgent.includes('Opera')) {
+        browser = 'Opera';
+      }
+
+      // OSの判定
+      if (userAgent.includes('Win')) {
+        os = 'Windows';
+      } else if (userAgent.includes('Mac')) {
+        os = 'macOS';
+      } else if (userAgent.includes('Linux')) {
+        os = 'Linux';
+      } else if (userAgent.includes('Android')) {
+        os = 'Android';
+      } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
+        os = 'iOS';
+      }
+      return { browser, os };
+    }
+  },
   async mounted() {
+    this.userAgent = navigator.userAgent;
+    this.language = navigator.language;
+    this.screenWidth = window.screen.width;
+    this.screenHeight = window.screen.height;
+    this.viewportWidth = window.innerWidth;
+    this.viewportHeight = window.innerHeight;
+    const { browser, os } = this.parseUserAgent();
+    this.browser = browser;
+    this.os = os;
     try {
       const response = await fetch('https://api.ipify.org?format=json');
       const data = await response.json();
