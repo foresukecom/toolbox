6718953fd07f7def7394e702233c27942c6359b3
foresuke <foresukecom@gmail.com>
2023-04-24 07:59:09 +0000

    feat: カウントダウンタイマーのスタイル調整

--- src/views/CountdownToHolidays.vue
+++ src/views/CountdownToHolidays.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <h1>Countdown to Holidays</h1>
-    <div v-for="(holiday, index) in upcomingHolidays" :key="index" class="holiday-container">
-      <div class="holiday-name">{{ holiday.name }}</div>
-      <div class="holiday-date">{{ holiday.date }}</div>
-      <div class="countdown">{{ getCountdownText(holiday.date) }}</div>
+  <div class="countdown-container">
+    <h1>祝日までのカウントダウン</h1>
+    <div class="timeline">
+      <div
+        v-for="(holiday, index) in upcomingHolidays"
+        :key="index"
+        class="timeline-item"
+      >
+        <div class="holiday-label">{{ holiday.name }}</div>
+        <div class="holiday-date">{{ holiday.date }}</div>
+        <div class="countdown-timer">{{ countdown(holiday.date) }}</div>
+      </div>
     </div>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios';
@@ -16,92 +23,97 @@ export default {
   data() {
     return {
       holidays: [],
-      timer: null,
+      upcomingHolidays: [],
+      countdownInterval: null,
     };
   },
-  computed: {
-    upcomingHolidays() {
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-      const filteredHolidays = this.holidays.filter(holiday => {
-        const holidayDate = new Date(holiday.date);
-        return holidayDate >= today;
-      });
-
-      const uniqueHolidays = [...new Map(filteredHolidays.map(h => [h.date, h])).values()];
-      uniqueHolidays.sort((a, b) => new Date(a.date) - new Date(b.date));
-      return uniqueHolidays.slice(0, 20);
-    }
-  },
   methods: {
-    getCountdownText(date) {
+    async fetchHolidays() {
+      const currentYear = new Date().getFullYear();
+      const nextYear = currentYear + 1;
+      const response = await axios.get(
+        `https://holidays-jp.github.io/api/v1/${currentYear}/date.json`
+      );
+      const responseNextYear = await axios.get(
+        `https://holidays-jp.github.io/api/v1/${nextYear}/date.json`
+      );
+      this.holidays = { ...response.data, ...responseNextYear.data };
+      this.upcomingHolidays = this.getUpcomingHolidays();
+    },
+    getUpcomingHolidays() {
       const now = new Date();
-      const holidayDate = new Date(date);
-      const diff = holidayDate - now;
+      return Object.entries(this.holidays)
+        .map(([date, name]) => ({ date, name }))
+        .filter(({ date }) => new Date(date) >= now)
+        .slice(0, 20);
+    },
+    countdown(targetDate) {
+      const now = new Date();
+      const target = new Date(targetDate);
+      const diff = target - now;
+
+      if (diff < 0) {
+        return '祝日が過ぎました';
+      }
 
       const days = Math.floor(diff / (1000 * 60 * 60 * 24));
       const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-      return `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds left`;
-    },
-    updateCountdown() {
-      this.timer = setInterval(() => {
-        this.$forceUpdate();
-      }, 1000);
+      return `${days}日 ${hours}時間 ${minutes}分 ${seconds}秒`;
     },
   },
   async mounted() {
-    const currentYear = new Date().getFullYear();
-    const nextYear = currentYear + 1;
-    try {
-      const responseCurrentYear = await axios.get(
-        `https://holidays-jp.github.io/api/v1/date.json?year=${currentYear}&count=20`
-      );
-      const responseNextYear = await axios.get(
-        `https://holidays-jp.github.io/api/v1/date.json?year=${nextYear}&count=20`
-      );
-
-      const holidaysCurrentYear = Object.entries(responseCurrentYear.data).map(([date, name]) => ({
-        date,
-        name,
-      }));
-
-      const holidaysNextYear = Object.entries(responseNextYear.data).map(([date, name]) => ({
-        date,
-        name,
-      }));
-
-      this.holidays = holidaysCurrentYear.concat(holidaysNextYear);
-      this.updateCountdown();
-    } catch (error) {
-      console.error('Error fetching holidays:', error);
-    }
+    await this.fetchHolidays();
+    this.countdownInterval = setInterval(() => {
+      this.$forceUpdate();
+    }, 1000);
   },
-  beforeDestroy() {
-    clearInterval(this.timer);
+  beforeUnmount() {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+      this.countdownInterval = null;
+    }
   },
 };
 </script>
 
 <style scoped>
-.holiday-container {
+.countdown-container {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   align-items: center;
-  padding: 1rem;
+}
+
+.timeline {
+  display: flex;
+  flex-direction: column;
+}
+
+.timeline-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
-  margin-bottom: 1rem;
+  margin-bottom: 10px;
 }
 
-.holiday-name {
+.holiday-label {
+  font-size: 18px;
   font-weight: bold;
 }
 
-.countdown {
-  font-weight: bold;
-  color: #f44336;
+.holiday-date {
+  font-size: 16px;
+  margin-left: 10px;
+}
+
+.countdown-timer {
+  font-size: 14px;
+  margin-left: 10px;
 }
 </style>
