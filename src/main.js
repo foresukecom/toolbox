793fc8abb0c66b5cc59a62320460d3ce995b8bde
foresuke <foresukecom@gmail.com>
2023-04-25 00:05:39 +0000

    add .env

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router';
 import { createVuetify } from 'vuetify';
 import '@mdi/font/css/materialdesignicons.css';
 
+console.log(`App author: ${process.env.VUE_APP_AUTHOR}`);
+
 const vuetify = createVuetify({
   icons: {
     iconfont: 'mdi',
