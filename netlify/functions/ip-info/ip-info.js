a8e1c50f17512970109a650bc230bf057e771437
foresuke <foresukecom@gmail.com>
2023-05-06 05:02:49 +0000

    wip: ip info

--- netlify/functions/ip-info/ip-info.js
+++ netlify/functions/ip-info/ip-info.js
@@ -1,19 +1,39 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
+const axios = require("axios");
+
+exports.handler = async function(event, context) {
+  const { ip } = event.queryStringParameters;
+
+  if (!ip) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "IP address is required." }),
+    };
+  }
+
+  const API_KEY = process.env.IPINFO_API_TOKEN;
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    console.log('event', event)
-    console.log(process.env.TITLE)
+    const response = await axios.get(`https://ipinfo.io/${ip}?token=${API_KEY}`);
+    const data = response.data;
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `🚗 Hello ${subject}, ${process.env.TITLE}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+      body: JSON.stringify({
+        ip: data.ip,
+        city: data.city,
+        region: data.region,
+        country: data.country,
+        postal: data.postal,
+        location: data.loc,
+        org: data.org,
+      }),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+    console.error("Error fetching IP information:", error);
 
-module.exports = { handler }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch IP information." }),
+    };
+  }
+};
