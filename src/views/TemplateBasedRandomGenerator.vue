7954fd3af2cc67d4f91b4bf48b86f764297d77df
foresuke <foresukecom@gmail.com>
2023-06-11 12:33:13 +0900

    feature: 乱数ジェネレーターからVuetifyを除外

--- src/views/TemplateBasedRandomGenerator.vue
+++ src/views/TemplateBasedRandomGenerator.vue
@@ -1,37 +1,40 @@
 <template>
-  <div>
-    <h1>Template-Based Random Generator</h1>
-    <div>
-      <h3>Rules:</h3>
-      <ul>
-        <li>'d' represents a random digit (0-9).</li>
-        <li>'l' represents a random lowercase letter (a-z).</li>
-        <li>'L' represents a random uppercase letter (A-Z).</li>
-        <li>'*' represents a random alphanumeric character (a-z, A-Z, 0-9).</li>
+  <div class="w-2/3 m-auto mt-10">
+    <h1 class="text-3xl font-bold mb-8">テンプレートに基づくランダムジェネレーター</h1>
+    <div class="mb-6">
+      <h3 class="font-bold mb-2">ルール:</h3>
+      <ul class="list-disc list-inside">
+        <li>'d' はランダムな数字 (0-9) を表します。</li>
+        <li>'l' はランダムな小文字 (a-z) を表します。</li>
+        <li>'L' はランダムな大文字 (A-Z) を表します。</li>
+        <li>'*' はランダムな英数字 (a-z, A-Z, 0-9) を表します。</li>
       </ul>
     </div>
-    <v-textarea label="Input Template" auto-grow v-model="inputTemplate" @input="generateRandomString"></v-textarea>
-    <v-textarea label="Generated String" auto-grow v-model="generatedString" readonly></v-textarea>
-    <v-btn @click="copyToClipboard">Copy to Clipboard</v-btn>
-    <v-snackbar v-model="showSnackbar" :timeout="1000" color="success">
-      コピーしました
-    </v-snackbar>
+    <label class="block mb-2">入力テンプレート</label>
+    <textarea class="mb-4 w-full h-24 p-2 border rounded" v-model="inputTemplate" @input="generateRandomString"></textarea>
+    <div class="flex items-center mb-2">
+      <label class="mr-2">生成された文字列:</label>
+      <CopyButton :textToCopy="generatedString" class="ml-2"/>
+    </div>
+    <textarea class="w-full h-24 p-2 border rounded bg-gray-100" v-model="generatedString" readonly></textarea>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import CopyButton from '@/components/CopyButton.vue';
+
 export default {
-  data() {
-    return {
-      inputTemplate: "",
-      generatedString: "",
-      showSnackbar: false,
-    };
+  components: {
+    CopyButton,
   },
-  methods: {
-    generateRandomString() {
+  setup() {
+    const inputTemplate = ref("");
+    const generatedString = ref("");
+
+    const generateRandomString = () => {
       const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-      this.generatedString = this.inputTemplate.split('').map((char) => {
+      generatedString.value = inputTemplate.value.split('').map((char) => {
         switch (char) {
           case 'd':
             return Math.floor(Math.random() * 10);
@@ -45,11 +48,12 @@ export default {
             return char;
         }
       }).join('');
-    },
-    async copyToClipboard() {
-      await navigator.clipboard.writeText(this.generatedString);
-      this.showSnackbar = true;
-    },
+    };
+
+    return { inputTemplate, generatedString, generateRandomString };
   },
 };
 </script>
+
+<style scoped>
+</style>
