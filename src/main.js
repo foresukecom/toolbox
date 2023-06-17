9d2a5bbd017223d894a76082a4bbd2dc53b13015
foresuke <foresukecom@gmail.com>
2023-06-17 17:21:39 +0900

    feature: GoogleAnalytic 設定

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/css/main.css';
+import VueGtag from 'vue-gtag'; // 追加
 
 console.log(
   `App author: ${process.env.VUE_APP_AUTHOR}, Git commit hash: ${process.env.VUE_APP_GIT_COMMIT_HASH}`
 );
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+
+// Google Analytics の設定を追加
+app.use(VueGtag, {
+  config: { id: process.env.VUE_APP_GA_TRACKING_ID } // あなたのGoogle Analytics 4の測定ID
+});
+
+app.mount('#app');
