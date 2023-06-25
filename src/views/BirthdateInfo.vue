019e42a5ce67341da234e1f969449355c70b40e4
foresuke <foresukecom@gmail.com>
2023-06-25 16:54:23 +0900

    fix: レイアウト修正

--- src/views/BirthdateInfo.vue
+++ src/views/BirthdateInfo.vue
@@ -1,44 +1,50 @@
 <template>
   <div>
-    <label>生年月日: </label>
-    <input type="date" v-model="birthdate" @change="calculate()" />
-    <div v-if="result">
-      <InfoCard title="星座" :text="result.zodiac" :showCopyButton="false" />
-      <InfoCard title="干支" :text="result.zodiacSign" :showCopyButton="false" />
-      <InfoCard title="曜日" :text="result.dayOfWeek" :showCopyButton="false" />
-      <InfoCard title="小学校"
-        :text="`${result.primarySchoolAdmissionYear}年4月〜${result.primarySchoolGraduationYear}年3月
-                              (${toJapaneseEra(result.primarySchoolAdmissionYear)}年4月〜${toJapaneseEra(result.primarySchoolGraduationYear)}年3月)`"
-        :showCopyButton="false" />
-      <InfoCard title="中学校"
-        :text="`${result.middleSchoolAdmissionYear}年4月〜${result.middleSchoolGraduationYear}年3月
-                            (${toJapaneseEra(result.middleSchoolAdmissionYear)}年4月〜${toJapaneseEra(result.middleSchoolGraduationYear)}年3月)`"
-        :showCopyButton="false" />
-      <InfoCard title="高校"
-        :text="`${result.highSchoolAdmissionYear}年4月〜${result.highSchoolGraduationYear}年3月
-                            (${toJapaneseEra(result.highSchoolAdmissionYear)}年4月〜${toJapaneseEra(result.highSchoolGraduationYear)}年3月)`"
-        :showCopyButton="false" />
-      <InfoCard title="大学" :text="`${result.universityAdmissionYear}年4月〜${result.universityGraduationYear}年3月
-        (${toJapaneseEra(result.universityAdmissionYear)}年4月〜${toJapaneseEra(result.universityGraduationYear)}年3月)`"
-        :showCopyButton="false" />
-      <label>大学・専門学校の制度: </label>
-      <select v-model="universityDuration" @change="calculate()">
-        <option value="2">2年制</option>
-        <option value="3">3年制</option>
-        <option value="4" selected>4年制</option>
-        <option value="6">6年制（大学院含む）</option>
-      </select>
+    <div style="display: flex; justify-content: center;">
+      <div style="width: 33%;">
+        <label>生年月日: </label>
+        <input type="date" v-model="birthdate" @change="calculate()" />
+        <div v-if="result">
+          <InfoCard title="星座" :text="result.zodiac" :showCopyButton="false" />
+          <InfoCard title="干支" :text="result.zodiacSign" :showCopyButton="false" />
+          <InfoCard title="曜日" :text="result.dayOfWeek" :showCopyButton="false" />
+          <InfoCard title="小学校"
+            :text="`${result.primarySchoolAdmissionYear}年4月〜${result.primarySchoolGraduationYear}年3月
+                                                                  (${toJapaneseEra(result.primarySchoolAdmissionYear)}年4月〜${toJapaneseEra(result.primarySchoolGraduationYear)}年3月)`"
+            :showCopyButton="false" />
+          <InfoCard title="中学校"
+            :text="`${result.middleSchoolAdmissionYear}年4月〜${result.middleSchoolGraduationYear}年3月
+                                                                (${toJapaneseEra(result.middleSchoolAdmissionYear)}年4月〜${toJapaneseEra(result.middleSchoolGraduationYear)}年3月)`"
+            :showCopyButton="false" />
+          <InfoCard title="高校"
+            :text="`${result.highSchoolAdmissionYear}年4月〜${result.highSchoolGraduationYear}年3月
+                                                                (${toJapaneseEra(result.highSchoolAdmissionYear)}年4月〜${toJapaneseEra(result.highSchoolGraduationYear)}年3月)`"
+            :showCopyButton="false" />
+          <label>大学・専門学校の制度: </label>
+          <select v-model="universityDuration" @change="calculate()">
+            <option value="2">2年制</option>
+            <option value="3">3年制</option>
+            <option value="4" selected>4年制</option>
+            <option value="6">6年制（大学院含む）</option>
+          </select>
+
+          <InfoCard title="大学"
+            :text="`${result.universityAdmissionYear}年4月〜${result.universityGraduationYear}年3月
+                                          (${toJapaneseEra(result.universityAdmissionYear)}年4月〜${toJapaneseEra(result.universityGraduationYear)}年3月)`"
+            :showCopyButton="false" />
 
 
-      <InfoCard title="社会人歴" :text="result.workExperience.toString() + ' 年'" :showCopyButton="false" />
-      <InfoCard title="成人年" :text="result.adultYear.toString()" :showCopyButton="false" />
-      <InfoCard title="還暦年" :text="result.kanrekiYear.toString()" :showCopyButton="false" />
-      <InfoCard title="喜寿年" :text="result.kijuYear.toString()" :showCopyButton="false" />
-      <InfoCard title="米寿年" :text="result.beijuYear.toString()" :showCopyButton="false" />
-      <InfoCard title="卒寿年" :text="result.sotsujuYear.toString()" :showCopyButton="false" />
-      <InfoCard title="白寿年" :text="result.hakujuYear.toString()" :showCopyButton="false" />
-      <InfoCard title="傘寿年" :text="result.sanjuYear.toString()" :showCopyButton="false" />
-      <InfoCard title="古希年" :text="result.kokiYear.toString()" :showCopyButton="false" />
+          <InfoCard title="社会人歴" :text="result.workExperience.toString() + ' 年'" :showCopyButton="false" />
+          <InfoCard title="成人年" :text="result.adultYear.toString()" :showCopyButton="false" />
+          <InfoCard title="還暦年" :text="result.kanrekiYear.toString()" :showCopyButton="false" />
+          <InfoCard title="喜寿年" :text="result.kijuYear.toString()" :showCopyButton="false" />
+          <InfoCard title="米寿年" :text="result.beijuYear.toString()" :showCopyButton="false" />
+          <InfoCard title="卒寿年" :text="result.sotsujuYear.toString()" :showCopyButton="false" />
+          <InfoCard title="白寿年" :text="result.hakujuYear.toString()" :showCopyButton="false" />
+          <InfoCard title="傘寿年" :text="result.sanjuYear.toString()" :showCopyButton="false" />
+          <InfoCard title="古希年" :text="result.kokiYear.toString()" :showCopyButton="false" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
