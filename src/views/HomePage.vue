bbf0c30424ff746a2389ab1506ab1ca955d5c0ee
foresuke <foresukecom@gmail.com>
2023-09-10 07:31:29 +0900

    feat: ランダムに採番されるタイトルカラーを固定化

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,8 +6,8 @@
       <div class="flex flex-wrap -mx-4">
         <div v-for="(tile, index) in category.tiles" :key="'tile-' + index" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 p-4">
           <div @click="goTo(tile.route)"
-            class="fusen text-center py-2 text-lg cursor-pointer transform transition hover:scale-105"
-            :style="{ 'border-left-color': getRandomColor() }">
+               class="fusen text-center py-2 text-lg cursor-pointer transform transition hover:scale-105"
+               :style="{ 'border-left-color': titleToColor(tile.title) }">
             {{ tile.title }}
           </div>
         </div>
@@ -145,14 +145,30 @@ export default {
     goTo(route) {
       this.$router.push({ name: route });
     },
-    getRandomColor() {
-      const letters = '0123456789ABCDEF';
+    // 文字列からハッシュを生成
+    stringToHash(str) {
+      let hash = 0;
+      for (let i = 0; i < str.length; i++) {
+        const char = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash |= 0; // Convert to 32bit integer
+      }
+      return hash;
+    },
+    // ハッシュから色を生成
+    hashToColor(hash) {
       let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+      for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xFF;
+        color += ('00' + value.toString(16)).substr(-2);
       }
       return color;
-    }
+    },
+    // タイトルから色を生成
+    titleToColor(title) {
+      const hash = this.stringToHash(title);
+      return this.hashToColor(hash);
+    },
   },
 };
 </script>
@@ -160,11 +176,11 @@ export default {
 
 <style>
 .fusen {
-    display: inline-block;
-    position: relative;
-    padding: .5em 1em;
-    border-left: 27px solid #2589d0;
-    background-color: #f5f5f5;
-    color: #333333;
+  display: inline-block;
+  position: relative;
+  padding: .5em 1em;
+  border-left: 27px solid #2589d0;
+  background-color: #f5f5f5;
+  color: #333333;
 }
 </style>
