afa466e223df1b33759f5e08b30d0cdae1ace2a9
foresuke <foresukecom@gmail.com>
2023-10-04 21:50:59 +0900

    fix: 未使用の変数削除

--- src/views/BirthdateInfo.vue
+++ src/views/BirthdateInfo.vue
@@ -68,10 +68,6 @@ export default {
   methods: {
     calculate() {
       const birthdate = new Date(this.birthdate);
-      const birthYear = birthdate.getFullYear();
-      const currentYear = new Date().getFullYear();
-      const universityAdmissionYear = birthYear + 19;
-      const universityGraduationYear = universityAdmissionYear + parseInt(this.universityDuration);
       const schoolDates = this.calculateSchoolDates(birthdate);
 
       const milestones = [
@@ -100,15 +96,6 @@ export default {
         zodiac: this.getZodiac(birthdate),
         eto: this.getEto(birthdate),
         dayOfWeek: Intl.DateTimeFormat("ja-JP", {weekday: "long"}).format(birthdate),
-        primarySchoolAdmissionYear: birthYear + 7,
-        primarySchoolGraduationYear: birthYear + 7 + 6,
-        middleSchoolAdmissionYear: birthYear + 7 + 6,
-        middleSchoolGraduationYear: birthYear + 7 + 6 + 3,
-        highSchoolAdmissionYear: birthYear + 7 + 6 + 3,
-        highSchoolGraduationYear: birthYear + 7 + 6 + 3 + 3,
-        universityAdmissionYear: universityAdmissionYear,
-        universityGraduationYear: universityGraduationYear,
-        workExperience: Math.max(0, currentYear - universityGraduationYear),
         milestones: milestones,
       };
       this.result = result;
