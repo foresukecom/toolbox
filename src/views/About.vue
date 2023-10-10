912f19e2167e90719d7c5d0aef3cc42d7b67b2a3
foresuke <foresukecom@gmail.com>
2023-10-11 06:40:13 +0900

    style: アバウトページのスタイル変更

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,34 +1,41 @@
-/* eslint-disable vue/multi-word-component-names */
-
 <template>
-  <div class="about">
-    <h1>About</h1>
-    <h2>Google Analyticsについてのプライバシーポリシー</h2>
-    <p>
+  <div class="about py-8 px-8">
+    <h1 class="text-2xl mb-2">About</h1>
+    <h2 class="text-xl mb-2">Google Analyticsについてのプライバシーポリシー</h2>
+    
+    <p class="mb-4">
       当サイトでは、サービス向上やサイトの利用状況を理解するために、Google LLCの提供するウェブ解析サービスであるGoogle Analyticsを利用しています。
     </p>
-    <p>
+    
+    <p class="mb-4">
       Google Analyticsは、当サイトが発行するクッキー（Cookie）を利用して、個々のユーザーを識別する情報を含みます。ただし、これら情報には個人を特定する情報は含まれません。収集された情報はGoogle LLCのサーバーへ送信・保存されます。
     </p>
-    <p>
+    
+    <p class="mb-4">
       Google Analyticsの利用により収集されたデータはGoogle LLCのプライバシーポリシーに基づいて管理されます。Google Analyticsにより収集されたデータの利用に関する説明はGoogle LLCのウェブサイトで確認できます。
     </p>
-    <p>
+    
+    <p class="mb-4">
       Google Analyticsのクッキーの使用に関する詳細な情報、及びこれを無効化する方法については以下のGoogle LLCのウェブサイトをご確認ください。
     </p>
-    <p>
-      <a href="https://www.google.com/intl/ja/policies/technologies/ads/" target="_blank" rel="noopener">Googleの広告に関するポリシー</a>
+    
+    <p class="mb-4">
+      <a href="https://www.google.com/intl/ja/policies/technologies/ads/" target="_blank" rel="noopener noreferrer" class="text-blue-500 underline">Googleの広告に関するポリシー</a>
     </p>
-    <p>
-      <a href="https://policies.google.com/privacy?hl=ja" target="_blank" rel="noopener">Googleのプライバシーポリシー</a>
+    
+    <p class="mb-4">
+      <a href="https://policies.google.com/privacy?hl=ja" target="_blank" rel="noopener noreferrer" class="text-blue-500 underline">Googleのプライバシーポリシー</a>
     </p>
-    <p>
+    
+    <p class="mb-4">
       Google Analyticsを使用しない設定については、以下のGoogle LLCが提供するページからGoogle Analytics Opt-out Browser Add-onをダウンロード・インストールすることでウェブブラウザから無効化することができます。
     </p>
-    <p>
-      <a href="https://tools.google.com/dlpage/gaoptout?hl=ja" target="_blank" rel="noopener">Google Analytics Opt-out Browser Add-on</a>
+    
+    <p class="mb-4">
+      <a href="https://tools.google.com/dlpage/gaoptout?hl=ja" target="_blank" rel="noopener noreferrer" class="text-blue-500 underline">Google Analytics Opt-out Browser Add-on</a>
     </p>
-    <p>
+    
+    <p class="mb-4">
       これらのリンク先はGoogle LLCのウェブサイトであり、当サイトはその内容について一切の責任を負いません。
     </p>
   </div>
@@ -39,28 +46,3 @@ export default {
   name: 'AboutPage'
 };
 </script>
-
-<style scoped>
-.about {
-  padding: 0 30px;
-}
-
-h1 {
-  font-size: 2em;
-  margin-bottom: 0.5em;
-}
-
-h2 {
-  font-size: 1.5em;
-  margin-bottom: 0.5em;
-}
-
-p {
-  margin-bottom: 1em;
-}
-
-a {
-  color: blue;
-  text-decoration: none;
-}
-</style>
