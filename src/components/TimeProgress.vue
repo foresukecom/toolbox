5af22fde9e2b9579330eabf853fa8b384545cb2d
foresuke <foresukecom@gmail.com>
2023-10-11 06:13:57 +0900

    style: プログレスバーをセンター表示

--- src/components/TimeProgress.vue
+++ src/components/TimeProgress.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="progress-container flex items-center h-25">
-    <div class="progress-value">{{ currentVal }}{{ unit }}</div>
-    <div class="progress mb-4 bg-gray-200 rounded-full relative dark:bg-gray-700" :class="barHeight">
-      <div class="progress-bar bg-blue-600 text-xs font-medium text-red-100 text-center flex items-center justify-center" :style="{ width: progressPercentage + '%', height: '100%' }">{{ progressPercentage }}%</div>
+  <div class="progress-container flex items-center w-3/4 h-50 mx-auto ">
+    <div class="progress-value w-24 text-right text-xl ">{{ currentVal }}{{ unit }}</div>
+    <div class="progress flex-grow mx-2.5 my-2 bg-gray-200 rounded-full relative dark:bg-gray-700 " :class="barHeight">
+      <div class="progress-bar h-15 bg-blue-600 text-xs font-medium text-red-100 text-center flex items-center justify-center " :style="{ width: progressPercentage + '%', height: '100%' }">{{ progressPercentage }}%</div>
     </div>
-    <div class="progress-value">{{ nextVal }}{{ unit }}</div>
+    <div class="progress-value w-24 text-right text-xl ">{{ nextVal }}{{ unit }}</div>
   </div>
 </template>
 
@@ -46,28 +46,4 @@ export default {
 </script>
 
 <style scoped>
-.progress-container {
-  display: flex;
-  align-items: center;
-  width: 75%;
-  height: 50px;
-  margin: 0 auto;
-}
-
-.progress-value {
-  width: 100px;
-  text-align: right; 
-  font-size: 25px; 
-}
-
-.progress {
-  flex-grow: 1;
-  margin: 0 10px;
-  background-color: #f5f5f5;
-}
-
-.progress-bar {
-  height: 60px;
-  background-color: #007bff;
-}
 </style>
