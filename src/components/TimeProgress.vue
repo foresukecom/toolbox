78aa22dbbd9aa91e46ed190a7bb9999a75011bb1
foresuke <foresukecom@gmail.com>
2023-10-10 16:06:54 +0900

    feat: プログレスバー内に進捗を表示

--- src/components/TimeProgress.vue
+++ src/components/TimeProgress.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="progress-container">
     <div class="progress-value">{{ currentVal }}{{ unit }}</div>
-    <div class="progress mb-4 bg-gray-200 rounded-full h-2/5 position-relative dark:bg-gray-700">
-      <div class="progress-bar bg-primary position-absolute" :style="{ width: progressPercentage + '%' }"></div>
+    <div class="progress mb-4 bg-gray-200 rounded-full h-5 position-relative dark:bg-gray-700">
+      <div class="progress-bar bg-blue-600 position-absolute text-xs font-medium text-red-100 text-center flex items-center justify-center" :style="{ width: progressPercentage + '%', height: '100%' }">{{ progressPercentage }}%</div>
     </div>
     <div class="progress-value">{{ nextVal }}{{ unit }}</div>
   </div>
@@ -36,7 +36,7 @@ export default {
   },
   computed: {
     progressPercentage() {
-      return (this.currentTime / this.totalTime) * 100;
+      return Math.floor((this.currentTime / this.totalTime) * 100);
     },
   }
 }
