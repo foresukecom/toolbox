b889ac2bed525754f002d7b3bcc9227c95958edb
foresuke <foresukecom@gmail.com>
2023-10-11 06:52:24 +0900

    feat: Aboutページ更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,48 +1,39 @@
 <template>
   <div class="about py-8 px-8">
     <h1 class="text-2xl mb-2">About</h1>
-    <h2 class="text-xl mb-2">Google Analyticsについてのプライバシーポリシー</h2>
 
+    <h2 class="text-xl mb-2 underline">このサイトについて</h2>
     <p class="mb-4">
-      当サイトでは、サービス向上やサイトの利用状況を理解するために、Google LLCの提供するウェブ解析サービスであるGoogle Analyticsを利用しています。
+      このサイトはWebで使用する便利機能をまとめたサイトになります。
     </p>
 
+    <h2 class="text-xl mb-2 underline">作者について</h2>
     <p class="mb-4">
-      Google Analyticsは、当サイトが発行するクッキー（Cookie）を利用して、個々のユーザーを識別する情報を含みます。ただし、これら情報には個人を特定する情報は含まれません。収集された情報はGoogle
-      LLCのサーバーへ送信・保存されます。
+      このサイトの作者に関する詳しい情報は、以下のプロフィールページでご確認いただけます。
+    </p>
+    <p class="mb-4">
+      <a href="https://foresuke.com/" target="_blank" rel="noopener noreferrer"
+        class="text-blue-500 underline">作者のプロフィールページ</a>
     </p>
 
+    <h2 class="text-xl mb-2 underline">Google Analyticsについてのプライバシーポリシー</h2>
     <p class="mb-4">
-      Google Analyticsの利用により収集されたデータはGoogle LLCのプライバシーポリシーに基づいて管理されます。Google Analyticsにより収集されたデータの利用に関する説明はGoogle
-      LLCのウェブサイトで確認できます。
+      当サイトはGoogle Analyticsを使用し、利用状況の解析を行っています。収集される情報は匿名で、個人を特定するものではありません。データはGoogleのプライバシーポリシーに基づき管理されます。
     </p>
-
     <p class="mb-4">
-      Google Analyticsのクッキーの使用に関する詳細な情報、及びこれを無効化する方法については以下のGoogle LLCのウェブサイトをご確認ください。
+      Google Analyticsのクッキーの詳細や無効化方法に関しては、Googleの公式サイトをご参照ください。
     </p>
-
     <p class="mb-4">
       <a href="https://www.google.com/intl/ja/policies/technologies/ads/" target="_blank" rel="noopener noreferrer"
         class="text-blue-500 underline">Googleの広告に関するポリシー</a>
     </p>
-
     <p class="mb-4">
       <a href="https://policies.google.com/privacy?hl=ja" target="_blank" rel="noopener noreferrer"
         class="text-blue-500 underline">Googleのプライバシーポリシー</a>
     </p>
-
-    <p class="mb-4">
-      Google Analyticsを使用しない設定については、以下のGoogle LLCが提供するページからGoogle Analytics Opt-out Browser
-      Add-onをダウンロード・インストールすることでウェブブラウザから無効化することができます。
-    </p>
-
     <p class="mb-4">
       <a href="https://tools.google.com/dlpage/gaoptout?hl=ja" target="_blank" rel="noopener noreferrer"
         class="text-blue-500 underline">Google Analytics Opt-out Browser Add-on</a>
-    </p>
-
-    <p class="mb-4">
-      これらのリンク先はGoogle LLCのウェブサイトであり、当サイトはその内容について一切の責任を負いません。
     </p>
   </div>
 </template>
