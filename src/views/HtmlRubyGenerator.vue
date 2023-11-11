978413c7a8b784481982c89c897caf8264ac1a4f
foresuke <foresukecom@gmail.com>
2023-11-11 22:40:55 +0900

    fix: ルビを表示するためのボタンを除去

--- src/views/HtmlRubyGenerator.vue
+++ src/views/HtmlRubyGenerator.vue
@@ -1,32 +1,24 @@
 <template>
   <div class="p-4">
     <div class="mb-4">
-      <div class="mb-4">
-        <label for="ruby" class="block text-gray-700 text-sm font-bold mb-2">ルビ:</label>
-        <input v-model="ruby" type="text" id="ruby"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-      </div>
-      <label for="kanji" class="block text-gray-700 text-sm font-bold mb-2">漢字:</label>
-      <input v-model="kanji" type="text" id="kanji"
+      <label for="ruby" class="block text-gray-700 text-sm font-bold mb-2">ルビ:</label>
+      <input v-model="ruby" type="text" id="ruby"
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
     </div>
     <div class="mb-4">
-      <button @click="generateHtml"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        HTMLで出力
-      </button>
+      <label for="kanji" class="block text-gray-700 text-sm font-bold mb-2">漢字:</label>
+      <input v-model="kanji" type="text" id="kanji"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
     </div>
     <div v-if="outputHtml" class="mt-4">
       <p class="text-gray-600">HTMLタグ付きテキスト:</p>
       <div v-html="outputHtml" class="text-blue-600"></div>
     </div>
-
-    <div v-if="outputHtml" class="mt-4">
-    <p class="text-gray-600">HTMLタグ付きテキスト:</p>
-    <!-- HTMLタグをテキストとして表示する -->
-    <div v-html="escapedHtml" class="text-blue-600"></div>
+    <div v-if="escapedHtml" class="mt-4">
+      <p class="text-gray-600">エスケープされたHTMLタグ:</p>
+      <div class="bg-gray-100 p-2 w-full" v-html="escapedHtml"></div>
+    </div>
   </div>
-  </div>  
 </template>
 
 <script>
@@ -36,18 +28,26 @@ export default {
       kanji: '',
       ruby: '',
       outputHtml: '',
-      escapedHtml: '', // エスケープされたHTMLを格納するためのプロパティ
+      escapedHtml: '',
     };
+  },
+  watch: {
+    kanji() {
+      this.generateHtml();
+    },
+    ruby() {
+      this.generateHtml();
+    }
   },
   methods: {
     generateHtml() {
       if (this.kanji && this.ruby) {
         const rawHtml = `<ruby>${this.kanji}<rt>${this.ruby}</rt></ruby>`;
         this.outputHtml = rawHtml;
-        this.escapedHtml = this.escapeHtml(rawHtml); // エスケープ処理を追加
+        this.escapedHtml = this.escapeHtml(rawHtml);
       } else {
         this.outputHtml = '';
-        this.escapedHtml = ''; // 入力がない場合は出力をクリア
+        this.escapedHtml = '';
       }
     },
     escapeHtml(html) {
