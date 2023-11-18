61a4ced0ab2491a345c2178ac556a370235b4f5b
foresuke <foresukecom@gmail.com>
2023-11-18 12:18:03 +0000

    💄 スタイルの指定方法変更

--- src/components/TextHighlighter.vue
+++ src/components/TextHighlighter.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="formatted-display">
-  <div v-if="label" class="label">{{ label }}</div>
-    <div class="text-area-container relative">
-      <pre class="formatted-text prose prose-sm max-w-none" v-html="highlightedText"></pre>
-      <CopyButton v-if="showCopyButton" class="copy-button absolute top-2 right-2" :textToCopy="formattedText" />
+  <div class="relative">
+    <div v-if="label" class="font-bold mb-2 text-lg">{{ label }}</div>
+    <div class="relative">
+      <div v-html="highlightedText" class="hljs min-h-[2rem]" ></div>
+      <CopyButton
+        v-if="showCopyButton"
+        class="absolute top-2 right-2"
+        :textToCopy="formattedText"
+      />
     </div>
   </div>
 </template>
@@ -42,27 +46,3 @@ export default {
 };
 </script>
 
-<style scoped>
-.formatted-display {
-  position: relative;
-}
-
-.text-area-container {
-  position: relative;
-}
-
-.formatted-text {
-  white-space: pre-wrap;
-  font-family: "Courier New", monospace;
-  background-color: #272822;
-  color: #f8f8f2;
-  padding: 16px;
-  border-radius: 4px;
-}
-
-.label {
-  /* ラベルのスタイリング例 */
-  font-weight: bold;
-  margin-bottom: 0.5em;
-}
-</style>
