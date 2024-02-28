19e7436df8661647141387721555b33db3a3b000
foresuke <foresukecom@gmail.com>
2024-02-28 13:57:58 +0000

    💄 表示スタイルの修正

--- src/views/SosuCounter.vue
+++ src/views/SosuCounter.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="(item, index) in displayedPrimes" :key="index">
-        {{ item.number }} ({{ item.time }})
-      </li>
-    </ul>
+  <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div v-for="(prime, index) in displayedPrimes" :key="index"
+      class="flex justify-center items-center h-auto max-w-full rounded-lg bg-blue-100 p-4 shadow-lg">
+      <span class="text-lg font-bold">{{ prime.number }}</span>
+    </div>
   </div>
 </template>
 
@@ -26,33 +25,35 @@ export default {
   },
   methods: {
     findNextPrime() {
+      const addPrime = () => {
+        if (this.isPrime(this.currentNumber)) {
+          // 配列の先頭に新しい素数を追加
+          this.displayedPrimes.unshift({ number: this.currentNumber });
+          return true;
+        }
+        return false;
+      };
       let found = false;
       while (!found) {
-        if (this.isPrime(this.currentNumber)) {
-          const now = new Date();
-          this.displayedPrimes.push({
-            number: this.currentNumber,
-            time: now.toLocaleTimeString(),
-          });
-          found = true;
-        }
+        found = addPrime();
         this.currentNumber++;
       }
-      this.adjustNextCall();
-    },
-    adjustNextCall() {
-      const now = new Date();
-      const nextCallDelay = 1000 - (now.getMilliseconds());
-      this.timeoutId = setTimeout(() => {
-        this.findNextPrime();
-      }, nextCallDelay);
+      setTimeout(() => this.findNextPrime(), 1000);
     },
     isPrime(num) {
       for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
         if (num % i === 0) return false;
       }
-      return num !== 1;
+      return num > 1;
     },
+  },
+
+  adjustNextCall() {
+    const now = new Date();
+    const nextCallDelay = 1000 - (now.getMilliseconds());
+    this.timeoutId = setTimeout(() => {
+      this.findNextPrime();
+    }, nextCallDelay);
   },
 };
 </script>
