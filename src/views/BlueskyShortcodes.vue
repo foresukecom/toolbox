ef05b558c8389f6779814181e5599548f7ee68a9
foresuke <foresukecom@gmail.com>
2024-05-09 21:57:47 +0900

    feat: 説明文をアコーディオンメニューで非表示に変更

--- src/views/BlueskyShortcodes.vue
+++ src/views/BlueskyShortcodes.vue
@@ -2,25 +2,37 @@
   <div>
     <h1>Bluesky 埋め込みコード</h1>
 
-    <h2>前提</h2>
-    これはHugoで生成するWebページにBlueskyの投稿内容を埋め込むための機能です。  
-    Blueskyの公式機能として、[WebページへBlueskyのポストを埋め込める機能](https://bsky.social/about/blog/post-embeds-guide)が追加されました。  
+    <div @click="toggleAccordion" class="accordion cursor-pointer p-2 bg-gray-200">
+      <h2 class="font-bold">前提</h2>
+    </div>
+    <div v-if="isOpen">
+      <p>
+        これはHugoで生成するWebページにBlueskyの投稿内容を埋め込むための機能です。
+      </p>
+      <p>
+         Blueskyの公式機能として、 <a href="https://bsky.social/about/blog/post-embeds-guide">WebページへBlueskyのポストを埋め込める機能</a>
+          が追加されました。
+      </p>
+      <p>
+        ただし埋め込み用のコードをそのまま貼り付けてもHugoで管理しているWebページでは表示されません。
+        なのでBlueskyのポスト表示用の
+        <a href="https://gohugo.io/templates/shortcode-templates/">カスタムショートコード</a>
+        を作成しました。
+        ショートコードは以下の通りです。
+      </p>
 
-    ただし埋め込み用のコードをそのまま貼り付けてもHugoで管理しているWebページでは表示されません。  
-    なのでBlueskyのポスト表示用の[カスタムショートコード](https://gohugo.io/templates/shortcode-templates/)を作成しました。  
-    ショートコードはこちらになります。  
-
-    <text-highlighter :formattedText="`<blockquote class='bluesky-embed' data-bluesky-uri='{{ .Get 'uri' | safeURL }}' data-bluesky-cid='{{ .Get 'cid' | safeHTMLAttr }}'>
+      <text-highlighter :formattedText="`<blockquote class='bluesky-embed' data-bluesky-uri='{{ .Get 'uri' | safeURL }}' data-bluesky-cid='{{ .Get 'cid' | safeHTMLAttr }}'>
   <p lang='ja'>{{ .Inner }}</p>&mdash; {{ .Get 'author' | safeHTML }}
   (<a href='{{ 'https://bsky.app/profile/' | safeURL }}{{ .Get 'user_uri' | safeURL }}?ref_src=embed'>@{{ .Get 'user_domain' | safeHTML }}</a>)
   <a href='{{ 'https://bsky.app/profile/' | safeURL }}{{ .Get 'user_uri' | safeURL }}/post/{{ .Get 'post_id' | safeURL }}?ref_src=embed'>{{ .Get 'date' | safeHTML }}</a>
 </blockquote>
 <script async src='https://embed.bsky.app/static/embed.js' charset='utf-8'></script>`"
-      highlightLanguage="html"></text-highlighter>
+        highlightLanguage="html"></text-highlighter>
 
-      このショートコードを呼び出すためのコードを、埋め込み用のコードから変換するためのページがこちらになります。  
+      このショートコードを呼び出すためのコードを、埋め込み用のコードから変換するためのページがこちらになります。
+    </div>
 
-      
+
     <textarea class="resize border rounded-md p-3 w-2/3" v-model="htmlInput" @input="generateShortcode"
       placeholder="Bluesky 埋め込みコード"></textarea>
 
@@ -39,12 +51,16 @@ export default {
       shortcodeOutput: '',
       errorMessages: "",
       hasError: false,
+      isOpen: false,
     };
   },
   components: {
     TextHighlighter,
   },
   methods: {
+    toggleAccordion() {
+      this.isOpen = !this.isOpen;
+    },
     generateShortcode() {
       // 新しいDOMParserインスタンスを作成
       const parser = new DOMParser();
