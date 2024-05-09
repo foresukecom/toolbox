1208565a160a7dfa1fe589b045632d68e232767f
foresuke <foresukecom@gmail.com>
2024-05-09 21:38:41 +0900

    docs: 説明追加

--- src/views/BlueskyShortcodes.vue
+++ src/views/BlueskyShortcodes.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
     <h1>Bluesky 埋め込みコード</h1>
+
+    <h2>前提</h2>
+    これはHugoで生成するWebページにBlueskyの投稿内容を埋め込むための機能です。  
+    Blueskyの公式機能として、[WebページへBlueskyのポストを埋め込める機能](https://bsky.social/about/blog/post-embeds-guide)が追加されました。  
+
+    ただし埋め込み用のコードをそのまま貼り付けてもHugoで管理しているWebページでは表示されません。  
+    なのでBlueskyのポスト表示用の[カスタムショートコード](https://gohugo.io/templates/shortcode-templates/)を作成しました。  
+    ショートコードはこちらになります。  
+
+    <text-highlighter :formattedText="`<blockquote class='bluesky-embed' data-bluesky-uri='{{ .Get 'uri' | safeURL }}' data-bluesky-cid='{{ .Get 'cid' | safeHTMLAttr }}'>
+  <p lang='ja'>{{ .Inner }}</p>&mdash; {{ .Get 'author' | safeHTML }}
+  (<a href='{{ 'https://bsky.app/profile/' | safeURL }}{{ .Get 'user_uri' | safeURL }}?ref_src=embed'>@{{ .Get 'user_domain' | safeHTML }}</a>)
+  <a href='{{ 'https://bsky.app/profile/' | safeURL }}{{ .Get 'user_uri' | safeURL }}/post/{{ .Get 'post_id' | safeURL }}?ref_src=embed'>{{ .Get 'date' | safeHTML }}</a>
+</blockquote>
+<script async src='https://embed.bsky.app/static/embed.js' charset='utf-8'></script>`"
+      highlightLanguage="html"></text-highlighter>
+
+      このショートコードを呼び出すためのコードを、埋め込み用のコードから変換するためのページがこちらになります。  
+
+      
     <textarea class="resize border rounded-md p-3 w-2/3" v-model="htmlInput" @input="generateShortcode"
       placeholder="Bluesky 埋め込みコード"></textarea>
 
@@ -57,4 +77,3 @@ ${p.textContent}
   },
 };
 </script>
-
